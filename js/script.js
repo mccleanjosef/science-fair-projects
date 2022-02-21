edec1e3d3752bd31c15d7e1f3cc920ac262ccacf
mccleanjosef <josefmcclean@gmail.com>
2022-02-21 18:00:18 +1300

    image-slider js commented out - jquery not working

--- js/script.js
+++ js/script.js
@@ -3,39 +3,39 @@ $('document').ready(function(){
   console.log("script is linked");
 
   // start of image-slider
-  const prev = document.querySelector('.prev');
-  const next = document.querySelector('.next');
-  const images = document.querySelector('.carousel').children;
-  const totalImages = images.length;
-  let index = 0;
+  // const prev = document.querySelector('.prev');
+  // const next = document.querySelector('.next');
+  // const images = document.querySelector('.carousel').children;
+  // const totalImages = images.length;
+  // let index = 0;
 
-  prev.addEventListener('click', () => {
-    nextImage('next');
-  })
+  // prev.addEventListener('click', () => {
+  //   nextImage('next');
+  // })
 
-  next.addEventListener('click', () => {
-    nextImage('prev');
-  })
+  // next.addEventListener('click', () => {
+  //   nextImage('prev');
+  // })
 
-  function nextImage(direction) {
-    if(direction == 'next') {
-      index++;
-      if(index == totalImages) {
-        index = 0;
-      }
-    } else {
-      if(index == 0) {
-        index = totalImages - 1;
-      } else {
-        index--;
-      }
-    }
+  // function nextImage(direction) {
+  //   if(direction == 'next') {
+  //     index++;
+  //     if(index == totalImages) {
+  //       index = 0;
+  //     }
+  //   } else {
+  //     if(index == 0) {
+  //       index = totalImages - 1;
+  //     } else {
+  //       index--;
+  //     }
+  //   }
 
-    for(let i = 0; i < images.length; i++) {
-      images[i].classList.remove('main');
-    }
-    images[index].classList.add('main');
-  }
+  //   for(let i = 0; i < images.length; i++) {
+  //     images[i].classList.remove('main');
+  //   }
+  //   images[index].classList.add('main');
+  // }
   // end of image-slider
 
 
